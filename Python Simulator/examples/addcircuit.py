66a81b45e0c69374d9f89874162c2e4be11f02a5
Arne <a.diehl@student.ru.nl>
2020-12-03 01:23:44 +0100

    Draw Graph, label nodes

--- Python Simulator/examples/addcircuit.py
+++ Python Simulator/examples/addcircuit.py
@@ -9,43 +9,44 @@ sim = inet['simulator']
 K=2
 
 # create single spike programmed input, spiking at t=0 only
-Programmed_one = net.createInputTrain([0,1], False) # the spike could be anything, but here it is of amplitude 2, not timestep 2
+Programmed_one = net.createInputTrain([0,1], False, ID="Programmed_one") # the spike could be anything, but here it is of amplitude 2, not timestep 2
 
 # create infinitely spiking neuron from t=1 onwards
-Const = net.createLIF(thr=1, V_reset=1, m=1)
-net.createSynapse(Programmed_one, Const, w=1, d=1)
+Const = net.createLIF(ID="Const", thr=1, V_reset=1, m=1)
+net.createSynapse(Programmed_one, Const, ID="Programmed_one-Const", w=1, d=1)
 
 # create clock which fire at point t=4 value 4
-Clock = net.createLIF(thr=K, V_reset=0, m=1)
-net.createSynapse(Const, Clock, w=1, d=1)
+Clock = net.createLIF(ID="Clock", thr=K, V_reset=0, m=1)
+net.createSynapse(Const, Clock, ID="Const-Clock", w=1, d=1)
 
 # create node Ah
-Ah = net.createLIF(thr=1, V_reset=1, m=0.5)
-net.createSynapse(Const, Ah, w=-5, d=K+2)
+Ah = net.createLIF(ID="Ah", thr=1, V_reset=1, m=0.5)
+net.createSynapse(Const, Ah, ID="Const-Ah", w=-5, d=K+2)
 
 # create node Bh
-Bh = net.createLIF(thr=1, V_reset=1, m=0.5)
-net.createSynapse(Const, Bh, w=-5, d=K+2)
+Bh = net.createLIF(ID="Ah", thr=1, V_reset=1, m=0.5)
+net.createSynapse(Const, Bh, ID="Const-Bh", w=-5, d=K+2)
 
 # create node Cin
-Cin = net.createLIF(thr=1, V_reset=1, m=0.5)
-net.createSynapse(Const, Cin, w=-5, d=K+2)
+Cin = net.createLIF(ID="Cin", thr=1, V_reset=1, m=0.5)
+net.createSynapse(Const, Cin, ID="Const-Cin", w=-5, d=K+2)
 
 # create Add node
-Add = net.createLIF(thr=K, V_reset=0, m=1)
-net.createSynapse(Ah, Add, w=1, d=1)
-net.createSynapse(Bh, Add, w=1, d=1)
-net.createSynapse(Cin, Add, w=1, d=3)
+Add = net.createLIF(ID="Add", thr=K, V_reset=0, m=1)
+net.createSynapse(Ah, Add, ID="Ah-Add", w=1, d=1)
+net.createSynapse(Bh, Add, ID="Bh-Add", w=1, d=1)
+net.createSynapse(Cin, Add, ID="Cin-Add", w=1, d=3)
 
 # create Cout node
-Cout = net.createLIF(thr=K, V_reset=0, m=1)
-net.createSynapse(Add, Cout, w=-1, d=1)
-net.createSynapse(Const, Cout, w=1, d=1)
+Cout = net.createLIF(ID="Cout", thr=K, V_reset=0, m=1)
+net.createSynapse(Add, Cout, ID="Add-Cout", w=-1, d=1)
+net.createSynapse(Const, Cout, ID="Const-Cout", w=1, d=1)
 
 # The two inputs and Cin, Low is encoded as d=2, High is encoded as d=1
-net.createSynapse(Clock, Ah, w=1, d=2)
-net.createSynapse(Clock, Bh, w=1, d=2)
-net.createSynapse(Clock, Cin, w=1, d=2)
+net.createSynapse(Clock, Ah, ID="Clock-Ah", w=1, d=2)
+net.createSynapse(Clock, Bh, ID="Clock-Bh", w=1, d=2)
+net.createSynapse(Clock, Cin, ID="Clock-Cin", w=1, d=2)
+
 
 Raster_1 = sim.createRaster()
 Raster_1.addTarget(Clock)
--- Python Simulator/pySimulator/networks.py
+++ Python Simulator/pySimulator/networks.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .nodes import *
 from .connections import *
+import networkx as nx
 
 class Network():
 	"""Network containing a list of nodes and synapses
@@ -16,9 +17,11 @@ class Network():
 	def __init__(self, nodes=[], synapses=[]):
 		self.nodes = nodes
 		self.synapses = synapses
+		self.graph = nx.DiGraph()
 
 
 	def createLIF(self, m, V_init=0, V_reset=0, V_min=0, thr=1, amplitude=1, I_e=0, noise=0, rng=None, ID=None, increment_count=True):
+		self.graph.add_node(ID)
 		node = LIF(m, V_init, V_reset, V_min, thr, amplitude, I_e, noise, rng, ID, increment_count)
 		self.nodes.append(node)
 		return node
@@ -34,6 +37,7 @@ class Network():
 		return node
 
 	def createSynapse(self, pre, post, w, d, ID=None, increment_count=True):
+		self.graph.add_edge(pre.ID, post.ID)
 		synapse = Synapse(pre, post, w, d, ID, increment_count)
 		self.synapses.append(synapse)
 		return synapse
